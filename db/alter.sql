2ab12611e232cbd328a3a78c95dca1058dda9b8f
theif45 <theif45@naver.com>
2023-02-06 17:50:31 +0900

    python markdown, review

--- db/alter.sql
+++ db/alter.sql
@@ -3,6 +3,10 @@ CREATE INDEX product_id on  product_mst(product_code);
 
 show index from product_mst;
 
+show full tables in db_study3 where table_type like 'VIEW';
+
+
+
 #unique 주기
 alter table product_mst
 add unique product_name(product_name);
@@ -13,4 +17,5 @@ drop index product_name2;
 #index 지우기
 alter table product_mst
 drop index product_id1;
+
 	
