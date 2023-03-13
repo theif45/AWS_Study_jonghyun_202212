b47c366f8c0095d1306b9510c026cb89b598dc46
theif45 <theif45@naver.com>
2023-03-13 12:52:19 +0900

    2023.03.13 css notice, js

---
+++ web_basic/javascript/js/js1.js
@@ -0,0 +1,14 @@
+// alert("알림창을 확인해주세요.");
+// alert("두번째 알림창");
+// alert(10 + 20);
+// console.log("println")
+// let flag = confirm("선택해주세요");
+// console.log(flag);
+// let name2 = prompt("이름을 입력해주세요");
+// console.log(name2);
+let test;
+console.log(test);
+
+let num = Number(prompt("첫번쩨 숫자를 입력하세요"));
+let num2 = parseInt(prompt("두번째 숫자를 입력하세요"));
+console.log(num + num2);
